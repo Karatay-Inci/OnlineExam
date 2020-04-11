db0c720f71b118f124c464cae73a2c674337cf40
inci-k <ikaratay@na.edu>
2020-04-11 14:37:05 -0500

    APRIL 11

---
+++ src/CODINGBAT/C1.java
@@ -0,0 +1,20 @@
+package CODINGBAT;
+
+public class C1 {
+       public static void main(String[] args) {
+           System.out.println(sleepIn(true,true));
+       }
+       public static boolean sleepIn(boolean weekday, boolean vacation) {
+           boolean sleepIn=true;
+           if(!weekday || vacation){
+               sleepIn=true;
+           }
+           else if(weekday||!vacation){
+               sleepIn=false;
+           }
+           else if(!weekday || !vacation){
+               sleepIn=true;
+           }
+           return sleepIn;
+       }
+}
