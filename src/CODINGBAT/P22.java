504a71353b745aa6407ec15d3305322d3f28a4e1
inci-k <ikaratay@na.edu>
2020-04-22 14:21:10 -0500

    APRIL 22

---
+++ src/CODINGBAT/P22.java
@@ -0,0 +1,19 @@
+package CODINGBAT;
+
+public class P22 {
+     public static void main (String [] args) {
+         message1();
+         message2();
+         System.out.println("5.");
+     }
+
+         public static void message1() {
+             System.out.println("1. (3.)");
+
+         }
+         public static void message2 () {
+             System.out.println("2.");
+             message1();// 3.
+             System.out.println("4.");
+         }
+}
