c030315b566bf568b8bb23a6438d0f5a58fb7a20
inci-k <ikaratay@na.edu>
2020-04-22 23:34:01 -0500

    APRIL 22

---
+++ src/CODINGBAT/P26.java
@@ -0,0 +1,26 @@
+package CODINGBAT;
+
+public class P26 {
+    public static void method1(){
+        System.out.println("I am method 1.");
+    }
+    public static void method2(){
+        method1();
+        System.out.println("I am method 2.");
+    }
+    public static void method3(){
+        method2();
+        System.out.println("I am method 3.");
+        method1();
+    }
+
+    public static void main(String[] args) {
+        method1();
+        System.out.println();
+        method3();
+        System.out.println();
+        method2();
+        System.out.println();
+        method3();
+    }
+}
