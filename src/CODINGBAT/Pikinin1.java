a652867b627bd154640fa465f92cc6727e61c40a
inci-k <ikaratay@na.edu>
2020-04-24 23:58:25 -0500

    APRIL 24

---
+++ src/CODINGBAT/Pikinin1.java
@@ -0,0 +1,10 @@
+package CODINGBAT;
+
+public class Pikinin1 {
+    public static void main (String [] args){
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
+
