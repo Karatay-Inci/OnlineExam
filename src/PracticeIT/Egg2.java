907b0325fc15622c62d843e16325054962cf222d
inci-k <ikaratay@na.edu>
2020-04-30 21:13:56 -0500

    APRIL 30

---
+++ src/PracticeIT/Egg2.java
@@ -0,0 +1,35 @@
+package PracticeIT;
+
+public class Egg2 {
+    public static void main(String[] args) {
+        method();
+        method();
+        method2();
+        method3();
+
+    }
+    public static void method(){
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+        System.out.println("-\"-'-\"-'-\"-");
+
+
+    }
+    public static void method2(){
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+
+    }
+    public static void method3(){
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+        System.out.println("-\"-'-\"-'-\"-");
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+
+    }
+}
