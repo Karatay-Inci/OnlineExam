dd142b9e62ccbbbb9013eb21fcd5b11f1730f3b8
inci-k <ikaratay@na.edu>
2020-05-20 07:35:40 -0500

    MAY 20

---
+++ src/PRACTICEitBOOK/A1ForLoopSquare.java
@@ -0,0 +1,42 @@
+package PRACTICEitBOOK;
+
+public class A1ForLoopSquare {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5 ; i++) {
+            System.out.println(i+" squared = "+(i*i));
+        }
+        System.out.println("------------------");
+
+        for (int j = -10; j <=10; j++) {
+            System.out.println(j+" squared = "+(j*j));
+        }
+        System.out.println("------------------");
+
+        for (int i = (2 + 2); i <= (4 * 3); i++) {
+            System.out.println(i + " squared = " + (i * i));
+        }
+        System.out.println("------------------");
+        System.out.println("------------------");
+
+        for (int i = 1; i <= 6; i++)        //pay attention   -->  {  }
+            System.out.println("Hi!");
+            System.out.println("Hello!");
+
+        System.out.println("------------------");
+        System.out.println("------------------");
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Hi!");
+            System.out.println("Hello!");
+        }
+        System.out.println("------------------");
+        System.out.println("------------------");
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Hi!");
+        }
+        System.out.println("Hello!");
+
+    }
+}
+// page 95
